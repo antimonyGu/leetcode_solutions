8bbca14bf8f42e20e86d00eb09ef56e22c6f6302
zzgu <zzgu@pixelshift.ai>
2024-11-21 20:23:26 -0800

    498. Diagonal Traverse

---
+++ 498.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        R, C = len(mat), len(mat[0])
+        diagonalDict = defaultdict(list)
+        # key - diagonal elements have the same r + c value
+        for r in range(R):
+            for c in range(C):
+                diagonalDict[r + c].append(mat[r][c])
+
+        ans = []
+        # ifs c + r is odd append to ans sequential, else reverse append to ans:
+        for i, value in enumerate(diagonalDict.values()):
+            if i % 2 == 0:
+                ans += value[::-1]
+            else:
+                ans += value
+        
+        return ans
+
